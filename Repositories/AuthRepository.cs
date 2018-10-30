424e73f07338c00a0f88f66bdd10bd3daa55f9fe
Martin Krisko <240314@VIAUC.DK>
2018-10-30 13:50:19 +0100

    New Inicialization

---
+++ Repositories/AuthRepository.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.ViewModels;
+
+namespace Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly BefordingTestContext _context;
+
+        public AuthRepository(BefordingTestContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Login(UserVM userVM)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == userVM.Email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user.Password == userVM.Password ? user : null;
+        }
+    }
+}
